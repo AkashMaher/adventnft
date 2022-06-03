20702e1bd70ff93f2fdf5717ce41dea3031b5517
Akash <aakashum121@gmail.com>
2022-06-03 06:03:24 +0530

    update: view users earned token

--- staking.js
+++ staking.js
@@ -128,7 +128,7 @@ async function onConnect() {
         EarnedTokens = web3.utils.fromWei(`${EarnedTokens}`, 'ether');
         EarnedTokens = parseFloat(EarnedTokens)
         EarnedTokens = EarnedTokens.toFixed(2)
-        document.getElementById("stakedBalance").textContent = `${EarnedTokens}`;
+        document.getElementById("stakedBalance").textContent = `Balance : ${EarnedTokens} $ADV`;
         console.log(EarnedTokens)
 
 
@@ -187,7 +187,7 @@ async function connectRefresh() {
         EarnedTokens = web3.utils.fromWei(`${EarnedTokens}`, 'ether');
         EarnedTokens = parseFloat(EarnedTokens)
         EarnedTokens = EarnedTokens.toFixed(2)
-        document.getElementById("stakedBalance").textContent = `${EarnedTokens}`;
+        document.getElementById("stakedBalance").textContent = `Balance : ${EarnedTokens} $ADV`;
         console.log(EarnedTokens)
 
 
